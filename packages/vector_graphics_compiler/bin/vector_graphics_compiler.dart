0ed340f816a236a03e859b23e37836950b365558
Jonah Williams <jonahwilliams@google.com>
2022-09-22 16:09:06 -0700

    Allow processing SVGs in parallel with Isolate.run

--- packages/vector_graphics_compiler/bin/vector_graphics_compiler.dart
+++ packages/vector_graphics_compiler/bin/vector_graphics_compiler.dart
@@ -3,9 +3,11 @@
 // found in the LICENSE file.
 
 import 'dart:io';
+import 'dart:isolate';
 import 'dart:typed_data';
 
 import 'package:args/args.dart';
+import 'package:pool/pool.dart';
 import 'package:vector_graphics_compiler/vector_graphics_compiler.dart';
 
 final ArgParser argParser = ArgParser()
@@ -41,6 +43,12 @@ final ArgParser argParser = ArgParser()
     help: 'Allows for overdraw optimizer to be enabled or disabled',
     defaultsTo: true,
   )
+  ..addOption(
+    'input-dir',
+    help: 'The path to a directory containing one or more SVGs. '
+        'Only includes files that end with .svg. '
+        'Cannot be combined with --input or --output.',
+  )
   ..addOption('input',
       abbr: 'i',
       help: 'The path to a file containing a single SVG',
@@ -67,6 +75,15 @@ void loadPathOpsIfNeeded(ArgResults results) {
   }
 }
 
+void validateOptions(ArgResults results) {
+  if (results.wasParsed('input-dir') &&
+      (results.wasParsed('input') || results.wasParsed('output'))) {
+    print(
+        '--input-dir cannot be combined with --input and/or --output options.');
+    exit(1);
+  }
+}
+
 Future<void> main(List<String> args) async {
   final ArgResults results;
   try {
@@ -76,6 +93,7 @@ Future<void> main(List<String> args) async {
     print(argParser.usage);
     exit(1);
   }
+  validateOptions(results);
 
   if (results['tessellate'] == true) {
     if (results.wasParsed('libtessellator')) {
@@ -89,10 +107,23 @@ Future<void> main(List<String> args) async {
 
   loadPathOpsIfNeeded(results);
 
-  final String inputFilePath = results['input'] as String;
-  final String xml = File(inputFilePath).readAsStringSync();
-  final File outputFile =
-      File(results['output'] as String? ?? '$inputFilePath.vg');
+  final List<Pair> pairs = <Pair>[];
+  if (results.wasParsed('--input-dir')) {
+    final Directory directory = Directory(results['input-dir'] as String);
+    for (final File file
+        in directory.listSync(recursive: true).whereType<File>()) {
+      if (!file.path.endsWith('.svg')) {
+        continue;
+      }
+      final String outputPath = '${file.path}.vg';
+      pairs.add(Pair(file.path, outputPath));
+    }
+  } else {
+    final String inputFilePath = results['input'] as String;
+    final String outputFilePath =
+        results['output'] as String? ?? '$inputFilePath.vg';
+    pairs.add(Pair(inputFilePath, outputFilePath));
+  }
 
   bool maskingOptimizerEnabled = true;
   bool clippingOptimizerEnabled = true;
@@ -110,12 +141,81 @@ Future<void> main(List<String> args) async {
     overdrawOptimizerEnabled = false;
   }
 
-  final Uint8List bytes = await encodeSvg(
-      xml: xml,
+  if (pairs.length == 1) {
+    final Uint8List bytes = await encodeSvg(
+      xml: File(pairs[0].inputPath).readAsStringSync(),
       debugName: args[0],
       enableMaskingOptimizer: maskingOptimizerEnabled,
       enableClippingOptimizer: clippingOptimizerEnabled,
-      enableOverdrawOptimizer: overdrawOptimizerEnabled);
+      enableOverdrawOptimizer: overdrawOptimizerEnabled,
+    );
 
-  outputFile.writeAsBytesSync(bytes);
+    File(pairs[0].outputPath).writeAsBytesSync(bytes);
+  } else {
+    final IsolateProcessor processor = IsolateProcessor();
+    await processor.process(
+      pairs,
+      maskingOptimizerEnabled: maskingOptimizerEnabled,
+      clippingOptimizerEnabled: clippingOptimizerEnabled,
+      overdrawOptimizerEnabled: overdrawOptimizerEnabled,
+    );
+  }
+}
+
+class IsolateProcessor {
+  final Pool pool = Pool(4);
+  int _total = 0;
+  int _current = 0;
+
+  Future<void> process(
+    List<Pair> pairs, {
+    required bool maskingOptimizerEnabled,
+    required bool clippingOptimizerEnabled,
+    required bool overdrawOptimizerEnabled,
+  }) async {
+    _total = pairs.length;
+    _current = 0;
+    await Future.wait(<Future<void>>[
+      for (Pair pair in pairs)
+        _process(
+          pair,
+          maskingOptimizerEnabled: maskingOptimizerEnabled,
+          clippingOptimizerEnabled: clippingOptimizerEnabled,
+          overdrawOptimizerEnabled: overdrawOptimizerEnabled,
+        )
+    ]);
+  }
+
+  Future<void> _process(
+    Pair pair, {
+    required bool maskingOptimizerEnabled,
+    required bool clippingOptimizerEnabled,
+    required bool overdrawOptimizerEnabled,
+  }) async {
+    PoolResource? resource;
+    try {
+      resource = await pool.request();
+      await Isolate.run(() async {
+        final Uint8List bytes = await encodeSvg(
+          xml: File(pair.inputPath).readAsStringSync(),
+          debugName: pair.inputPath,
+          enableMaskingOptimizer: maskingOptimizerEnabled,
+          enableClippingOptimizer: clippingOptimizerEnabled,
+          enableOverdrawOptimizer: overdrawOptimizerEnabled,
+        );
+        File(pair.outputPath).writeAsBytesSync(bytes);
+      });
+      _current++;
+      print('Progress: $_current/$_total');
+    } finally {
+      resource?.release();
+    }
+  }
+}
+
+class Pair {
+  const Pair(this.inputPath, this.outputPath);
+
+  final String inputPath;
+  final String outputPath;
 }

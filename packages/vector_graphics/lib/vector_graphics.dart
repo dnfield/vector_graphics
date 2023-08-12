401eea6b0ade838551bbf00e99c007b90b101ee1
Alex Li <github@alexv525.com>
2023-08-12 12:58:12 +0800

    Update vector_graphics.dart

--- packages/vector_graphics/lib/vector_graphics.dart
+++ packages/vector_graphics/lib/vector_graphics.dart
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+export 'src/html_render_vector_graphics.dart' show RenderWebVectorGraphic;
+export 'src/render_vector_graphics.dart' show RenderVectorGraphic;
 export 'src/vector_graphics.dart'
     show
         AssetBytesLoader,
